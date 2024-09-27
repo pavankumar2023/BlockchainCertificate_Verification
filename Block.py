2a9ee65efb554153007640b18436d192a81eaf6f
pavankumar2023 <125943939+pavankumar2023@users.noreply.github.com>
2024-09-27 21:34:54 +0530

    Initial commit

--- Block.py
+++ Block.py
@@ -16,4 +16,4 @@ class Block:
         A function that return the hash of the block contents.
         """
         block_string = json.dumps(self.__dict__, sort_keys=True)
-        return sha256(block_string.encode()).hexdigest()
+        return sha256(block_string.encode()).hexdigest() 
